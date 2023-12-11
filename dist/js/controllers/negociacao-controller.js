53154a7414a880ca90a169bae67e3f2acafd3845
Daniel <daniel502121@gmail.com>
2023-12-11 20:51:46 -0300

    first commit

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,30 @@
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+export class negociacaoController {
+    constructor() {
+        this.negociacoes = new Negociacoes();
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+    }
+    adicionar() {
+        const negociacao = this.criarNegociacao();
+        this.negociacoes.adicionar(negociacao);
+        console.log(this.negociacoes.lista());
+        console.log(negociacao);
+        this.limparNegociacao();
+    }
+    criarNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ","));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+    limparNegociacao() {
+        this.inputData.value = "";
+        this.inputQuantidade.value = "";
+        this.inputValor.value = "";
+        this.inputData.focus();
+    }
+}
